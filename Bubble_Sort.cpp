0c08f125673be87ff08f836f6834900d94404270
SudarshanAhire <sudarshanahire292@gmail.com>
2024-08-12 18:29:44 +0530

    commit2

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void Bubble_Sort(int arr[], int n){
 
-    for(int i=0; i<n; i++){
+    for(int i=1; i<n; i++){
         
-        for(int j=0; j<n-1; j++){
+        for(int j=0; j<n-i; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
             }
