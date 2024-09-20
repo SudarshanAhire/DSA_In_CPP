f153a4d3e7f409de787e2f0da5048fb0315859a0
SudarshanAhire <sudarshanahire292@gmail.com>
2024-09-21 01:28:06 +0530

    commit1

---
+++ print.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0; i<10; i++){
+        cout<<i;
+        for(int j=0; j<10; j++){
+            cout<<j;
+            // for(int k=0; k<10; K++){
+            //     cout<<k;
+            // }
+        }
+    }
+
+    return 0;
+}
