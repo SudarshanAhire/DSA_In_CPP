3335b5ff0cbe812b47eea6b264216439d8c74c78
SudarshanAhire <sudarshanahire292@gmail.com>
2024-09-27 22:46:35 +0530

    comment 1

--- 36_quickSort.cpp
+++ 36_quickSort.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 using namespace std;
 
+void quickSort(int arr[], int s, int e){
+
+    if(s <= e){
+        return ;
+    }
+
+    int p = partition(arr, s, e);
+
+    quickSort(arr, s, p-1);
+
+    quickSort(arr, p+1, e);
+    
+}
+
 int main(){
 
     int arr[5] = {7,5,9,4,2};
