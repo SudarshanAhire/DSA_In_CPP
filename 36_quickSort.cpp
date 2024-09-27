f4861e297e9f09981a518e8fb5e96166c9ed4bb6
SudarshanAhire <sudarshanahire292@gmail.com>
2024-09-27 22:43:48 +0530

    comment 1

--- 36_quickSort.cpp
+++ 36_quickSort.cpp
@@ -3,5 +3,14 @@ using namespace std;
 
 int main(){
 
+    int arr[5] = {7,5,9,4,2};
+    int n = 5;
+
+    quickSort(arr, 0, n-1);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }cout << endl;
+
     return 0;
 }
